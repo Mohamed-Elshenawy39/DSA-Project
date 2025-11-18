0faf4c0e1d8daf3d18d7792e33287d8219e1c79a
MyGit <mohamed.elshenawy39@gmail.com>
2025-11-18 21:05:04 +0200

    Final 2

--- DSA-Project/AbortedRequest.cpp
+++ DSA-Project/AbortedRequest.cpp
@@ -3,7 +3,8 @@
 void AbortRequest::Operate(MarsStation* station)
 {
     Missions* pMission = new Missions (this->missionID,MISSION_ABORT,this->requestDay,0,0,0);
-    station->addMission(pMission);
+    station->addMission(pMission); // Adds to the Aborted Missions List
+    station->abortMission(this->missionID);
 }
 
 void AbortRequest::print() const
